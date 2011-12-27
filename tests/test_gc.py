be6d87af9ca5f37aaeab9bff58336bbb9f49bf2f
Alexey Borzenkov <snaury@gmail.com>
2011-12-27 19:21:53 +0400

    Restore old full gc tests

--- tests/test_gc.py
+++ tests/test_gc.py
@@ -5,6 +5,59 @@ import weakref
 
 import greenlet
 
+def _live_greenlet_body():
+    g = greenlet.getcurrent()
+    try:
+        g.parent.switch(g)
+    finally:
+        pass
+
+def _switch_to_parent():
+    g = greenlet.getcurrent()
+    g.parent.switch(g)
+
+def _live_subframe_body():
+    g = greenlet.getcurrent()
+    try:
+        _switch_to_parent()
+    finally:
+        pass
+
+def _live_stub_body(g):
+    try:
+        g.parent.switch(g)
+    finally:
+        pass
+
+class _live_throw_exc(Exception):
+    def __init__(self, g):
+        self.greenlet = g
+
+def _live_throw_body(g):
+    try:
+        g.parent.throw(_live_throw_exc(g))
+    finally:
+        pass
+
+def _live_cluster_body(g):
+    o = weakref.ref(greenlet.greenlet(_live_greenlet_body).switch())
+    gc.collect()
+    try:
+        g.parent.switch(g)
+    finally:
+        pass
+
+def _make_green_weakref(body, kw=False):
+    g = greenlet.greenlet(body)
+    try:
+        if kw:
+            g = g.switch(g=g)
+        else:
+            g = g.switch(g)
+    except _live_throw_exc:
+        pass
+    return weakref.ref(g)
+
 class GCTests(unittest.TestCase):
     def test_circular_greenlet(self):
         class circular_greenlet(greenlet.greenlet):
@@ -33,6 +86,46 @@ class GCTests(unittest.TestCase):
                     pass
             o = inactive_greenlet()
             o = weakref.ref(o)
+            gc.collect()
+            self.assertTrue(o() is None)
+            self.assertFalse(gc.garbage, gc.garbage)
+
+    if greenlet.GREENLET_USE_GC and greenlet.GREENLET_USE_GC_FULL:
+        # These only work with greenlet full gc support
+
+        def test_live_circular_ref(self):
+            o = weakref.ref(greenlet.greenlet(_live_greenlet_body).switch())
+            gc.collect()
+            self.assertTrue(o() is None)
+            self.assertFalse(gc.garbage, gc.garbage)
+
+        def test_live_subframe_ref(self):
+            o = weakref.ref(greenlet.greenlet(_live_subframe_body).switch())
+            gc.collect()
+            self.assertTrue(o() is None)
+            self.assertFalse(gc.garbage, gc.garbage)
+
+        def test_stub_circular_ref(self):
+            o = _make_green_weakref(_live_stub_body)
+            gc.collect()
+            self.assertTrue(o() is None)
+            self.assertFalse(gc.garbage, gc.garbage)
+
+        def _disabled_test_stub_circular_kw_ref(self):
+            # Unfortunately keyword arguments cannot be traversed
+            o = _make_green_weakref(_live_stub_body, kw=True)
+            gc.collect()
+            self.assertTrue(o() is None)
+            self.assertFalse(gc.garbage, gc.garbage)
+
+        def test_stub_throw_ref(self):
+            o = _make_green_weakref(_live_throw_body)
+            gc.collect()
+            self.assertTrue(o() is None)
+            self.assertFalse(gc.garbage, gc.garbage)
+
+        def test_stub_cluster_ref(self):
+            o = _make_green_weakref(_live_cluster_body)
             gc.collect()
             self.assertTrue(o() is None)
             self.assertFalse(gc.garbage, gc.garbage)
